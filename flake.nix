be1bcce6396eaae08c55e1e8159ecd2ce1fc1456
Anakin Childerhose <anakin@childerhose.ca>
2026-03-14 21:34:23 -0400

    chore: add nix flake with bats, jq, and kconfig-language-server

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Language Server Test Suite – bats-based LSP testing library";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    kconfig-language-server.url = "github:anakin4747/kconfig-language-server";
+    kconfig-language-server.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, kconfig-language-server }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        kls = kconfig-language-server.packages.${system}.default;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          name = "lsts";
+
+          packages = with pkgs; [
+            bash   # ensure bash 4+ (important for coproc)
+            bats   # test runner
+            jq     # JSON parsing in tests
+            kls    # kconfig-language-server under test
+          ];
+
+          shellHook = ''
+            echo "lsts dev shell – bats $(bats --version) / jq $(jq --version)"
+          '';
+        };
+      });
+}
